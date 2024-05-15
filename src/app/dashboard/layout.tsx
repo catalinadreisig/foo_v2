a0c2a1acb1d28ab904e203c699f25f7c36cc6803
catalinadreisig <catalinadreisig@yahoo.dk>
2024-05-15 11:02:19 +0200

    3 timers video

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,12 +1,1 @@
-import Link from "next/link";
 
-export default function DashboardLayout({ children }: { children: React.ReactNode }) {
-  return;
-  <section className="space-y-4">
-    <nav className="flex  items-center justify-between">
-      <Link className="underline" href="/dashboard/about" about </Link>
-      <Link className="underline" href="/dashboard/ticket" buy ticket</Link>
-    </nav>
-    {children}
-  </section>;
-}
