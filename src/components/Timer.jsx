aba987af3a52b09697143e2bfd9c769806d916b4
catalinadreisig <catalinadreisig@yahoo.dk>
2024-05-30 09:26:43 +0200

    cata

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState, useEffect } from "react";
 export default function CountdownTimer() {
-  const [seconds, setSeconds] = useState(10);
+  const [seconds, setSeconds] = useState(60);
   const [timeOut, setTimeOut] = useState(false);
 
   const displaySeconds = seconds % 60;
