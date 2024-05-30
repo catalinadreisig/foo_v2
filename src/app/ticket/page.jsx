27af8c002c6b62224ce4997e8b1fdc936fb222fb
catalinadreisig <catalinadreisig@yahoo.dk>
2024-05-30 12:35:57 +0200

    supabase

--- src/app/ticket/page.jsx
+++ src/app/ticket/page.jsx
@@ -7,8 +7,8 @@ import { createClient } from "@supabase/supabase-js";
 
 //supabase
 const supabaseUrl = "https://tynqwkikafukcxlrdlur.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR5bnF3a2lrYWZ1a2N4bHJkbHVyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY5ODU0ODgsImV4cCI6MjAzMjU2MTQ4OH0.UBqEYSQzYOROuFFUt61qesvRl2aoPNbobWVl6sCr7Bk"; // Replace with your Anon Key
-const supabase = createClient("https://tynqwkikafukcxlrdlur.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR5bnF3a2lrYWZ1a2N4bHJkbHVyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY5ODU0ODgsImV4cCI6MjAzMjU2MTQ4OH0.UBqEYSQzYOROuFFUt61qesvRl2aoPNbobWVl6sCr7Bk"); // Erstat med din URL og Anon Key
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR5bnF3a2lrYWZ1a2N4bHJkbHVyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY5ODU0ODgsImV4cCI6MjAzMjU2MTQ4OH0.UBqEYSQzYOROuFFUt61qesvRl2aoPNbobWVl6sCr7Bk"; // Replace with your Anon Key
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default function Ticket() {
   const [regTickets, setRegTickets] = useState(0);
