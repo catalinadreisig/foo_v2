efb9e40d889df336747fb11d86f2c85e4ca8b43e
catalinadreisig <catalinadreisig@yahoo.dk>
2024-05-30 12:37:43 +0200

    supa

--- src/app/ticket/page.jsx
+++ src/app/ticket/page.jsx
@@ -8,7 +8,6 @@ import { createClient } from "@supabase/supabase-js";
 //supabase
 const supabaseUrl = "https://tynqwkikafukcxlrdlur.supabase.co";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR5bnF3a2lrYWZ1a2N4bHJkbHVyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY5ODU0ODgsImV4cCI6MjAzMjU2MTQ4OH0.UBqEYSQzYOROuFFUt61qesvRl2aoPNbobWVl6sCr7Bk"; // Replace with your Anon Key
-const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default function Ticket() {
   const [regTickets, setRegTickets] = useState(0);
@@ -22,6 +21,9 @@ export default function Ticket() {
   const [reservationId, setReservationId] = useState(false);
   const [resId, setResId] = useState();
   const [totalPrice, setTotalPrice] = useState(99);
+
+  //supabase
+  const supabase = createClient(supabaseUrl, supabaseKey);
 
   //might use:
   const [oldValue] = useState();
