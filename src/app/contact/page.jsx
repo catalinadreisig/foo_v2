25c74a6f01b8026d0014cddc042f7d7fadc7bf03
catalinadreisig <catalinadreisig@yahoo.dk>
2024-06-07 12:49:45 +0200

    h

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,16 +1,25 @@
 import React from "react";
+import Lines from "../../components/Lines";
 
 export default function contact() {
   return (
-    <section className="text-breads text-white uppercase">
-      <div>
-        <h1 className="text-headers pt-20 justify-self-end pb-8">phonenumber</h1>
-        <h1 className="text-headers pt-20 justify-self-end pb-8">mail</h1>
-        <h1 className="text-headers pt-20 justify-self-end pb-8">adress</h1>
+    <section className="text-breads text-white uppercase ">
+      <h1 className="text-headers pt-20 pb-10 text-end ">contact</h1>
+      <p className="text-links text-end">reach the foos @</p>
+      <Lines />
+      <div className="pb-6 md:grid md:grid-cols-3">
+        <h1 className="text-links pt-20 text-start ">phonenumber</h1>
+        <h1 className="text-links pt-20 text-center ">mail</h1>
+        <h1 className="text-links pt-20 text-end">adress</h1>
       </div>
-      <div>
-        <a href="tel:+4526707787">+4526707787</a>
-        <a href="mailto:+estherfinsen@gmail.com">estherfinsen@gmail.com</a>
+      <div className="md:grid md:grid-cols-3 ">
+        <a className="text-start" href="tel:+4526707787">
+          +4526707787
+        </a>
+        <a className="text-center" href="mailto:+estherfinsen@gmail.com">
+          foo@foo.dk
+        </a>
+        <p className="text-end">foo street 12, 3450 foo city</p>
       </div>
     </section>
   );
