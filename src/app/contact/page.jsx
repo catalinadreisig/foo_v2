18d7dafe27f58699d64f7fb34194ed3c55f68f6d
catalinadreisig <catalinadreisig@yahoo.dk>
2024-06-07 12:20:26 +0200

    hej

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function contact() {
-  return <div>contact</div>;
+  return (
+    <section>
+      <div>
+        <h1 className="text-headers pt-20 justify-self-end pb-8">Tickets</h1>
+      </div>
+    </section>
+  );
 }
